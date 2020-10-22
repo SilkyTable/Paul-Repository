d1ebe87210a889ea5f6366591199958a781bfd56
SilkyTable <71032322+SilkyTable@users.noreply.github.com>
2020-10-21 20:38:10 -0400

    Add files via upload

---
+++ FractionTest.java
@@ -0,0 +1,12 @@
+public class FractionTest {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    	int num = 1;
+    	int dem = 2;
+        Fraction poo = new Fraction();
+        String print = poo.ans(num, dem);
+        System.out.println(print);
+    }
+
+}
